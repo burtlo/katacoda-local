9b8817059c33eb60f95a8692da19e9a5b458db66
Lynn Frank <franklin.webber@gmail.com>
2020-06-18 16:06:26 -0500

    Updates the title and index of files

--- vault-kubernetes-csi/courseBase.sh
+++ vault-kubernetes-csi/courseBase.sh
@@ -2,3 +2,6 @@
 curl -LO https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz
 tar -xvf helm-v3.2.1-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/bin/
+
+# Install a shallow copy of the secrets-store-csi-driver
+git clone --depth=1 https://github.com/kubernetes-sigs/secrets-store-csi-driver.git
