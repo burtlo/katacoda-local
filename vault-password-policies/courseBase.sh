da727dc3e2f9ba6f10c309196264ebd5dab72970
Lynn Frank <franklin.webber@gmail.com>
2020-07-15 02:13:02 -0500

    changes the coursebase for password policy

--- vault-password-policies/courseBase.sh
+++ vault-password-policies/courseBase.sh
@@ -1,10 +1,15 @@
-# RabbitMQ: install and start the server
-sudo apt-get -y install rabbitmq-server --fix-missing
-sudo service rabbitmq-server start
+# Run RabbitMQ in docker with:
+#   - standard HTTP admin management enabled
+#   - standard HTTP port
+#   - user named 'learn_vault'
+#   - password 'hashicorp'
 
-# RabbitMQ: enable HTTP management port
-sudo rabbitmq-plugins enable rabbitmq_management
+docker run --rm --name some-rabbit -p 15672:15672 \
+    -e RABBITMQ_DEFAULT_USER=learn_vault \
+    -e RABBITMQ_DEFAULT_PASS=hashicorp \
+    rabbitmq:3-management
 
-# RabbitMQ: create user and add the administrator tag
-sudo rabbitmqctl add_user learn_vault hashicorp
-sudo rabbitmqctl set_user_tags learn_vault administrator
+# Vault: install custom-built Vault 1.5 binary
+wget https://lynn-vault-binaries.s3.us-east-2.amazonaws.com/vault
+sudo mv ./vault /usr/local/bin/vault
+sudo chmod a+x /usr/local/bin/vault
