480ab7b6e34fda0eed6bcdea276dec35441b0fa2
Lynn Frank <franklin.webber@gmail.com>
2020-07-21 12:04:45 -0500

    Updates verification step for external vault

--- vault-kubernetes-external-vault/step1.verify.sh
+++ vault-kubernetes-external-vault/step1.verify.sh
@@ -1,10 +1,16 @@
 VAULT_ADDR=http://0.0.0.0:8200
+vault login root
 
-if [[ $(vault read -format json secret/data/devwebapp/config) ]]
+vault read -format json secret/data/devwebapp/config
+secretRead=$?
+
+if [[ $secretRead -eq 0 ]]
 then
   echo "done"
+  exit 0
 else
   echo "Vault is not running with secret"
+  exit 1
 fi
 
 
